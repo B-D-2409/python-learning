d0369d594274681f1a532b1ecb80f09847495d7d
Masterclass777 <borislav.2404g@gmail.com>
2025-10-18 11:56:11 +0300

    first 3 days are done.

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,28 @@
-len(12345)
+from math import floor
+
+len("Hello")
+
+print(type(123))
+
+
+
+print(type('Hello'))
+print(type(123))
+print(type(3.14))
+print(type(True))
+
+
+
+print(int('123') + int('345'))
+
+float()
+int()
+str()
+bool()
+
+name_of_the_user = input("Number of letters in your name: ")
+length_of_the_name = len(name_of_the_user)
+print(type("Number of letters in your name: "))
+print(type(length_of_the_name))
+
+print("Number of letters in your name: " + str(length_of_the_name))
---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,8 @@
+current_number = input("Fill the number")
+
+converted_to_number = int(current_number)
+
+if converted_to_number % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")

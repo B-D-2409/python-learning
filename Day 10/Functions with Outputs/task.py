fffde386851cb3750e3a4378b2c2e683eccf8982
Masterclass777 <borislav.2404g@gmail.com>
2025-10-29 11:42:03 +0200

    day -10

---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,23 @@
+def format_name(f_name,l_name):
+   formated_f_name = f_name.title()
+   formated_l_name = l_name.title()
+   return f"{formated_f_name} {formated_l_name}"
+
+
+
+print(format_name("AnGela", "YU"))
+
+output = len("Angela")
+
+
+
+def function_1(text):
+    return text + text
+
+
+def function_2(text):
+    return text.title()
+
+
+output2 = function_2(function_1("hello"))
+print(output2)

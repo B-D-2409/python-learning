dd9ed888bf8e7c07eea651c60e51c779efd3550c
Masterclass777 <borislav.2404g@gmail.com>
2025-11-10 09:37:16 +0200

    day-17

---
+++ Day 16-100/day-17-start/main.py
@@ -0,0 +1,24 @@
+class User:
+
+
+    def __init__(self,user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+
+    def follow(self, user):
+        user.followers += 1
+        user.following += 1
+
+
+
+
+user_1 = User("001", "angela")
+user_2 = User("002", "Bobi")
+
+
+user_1.follow(user_2)
+
+
